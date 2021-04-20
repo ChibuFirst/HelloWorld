ffe4763d59bb3f7413060647423c0cb3f815b156
ChibuFirst <chibuzoonuoha57@gmail.com>
2021-04-20 05:43:13 +0100

    Added views and code for the increment

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textNumber: TextView = findViewById(R.id.textNumber)
+        val buttonIncrement: Button = findViewById(R.id.buttonIncrement)
+        buttonIncrement.setOnClickListener {
+            textNumber.text = incrementValue(textNumber.text.toString())
+        }
+    }
+
+    private fun incrementValue(number: String): String {
+        val num = number.toInt() + 1
+        return num.toString()
     }
 }
